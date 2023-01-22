20d216545676e1f0fad7bf58fe5a8fd434d8faac
stepanenkos10 <123330889+stepanenkos10@users.noreply.github.com>
2023-01-22 22:56:53 +0200

    Add files via upload

---
+++ papka/lang.ro.1006.js
@@ -0,0 +1,14 @@
+// Copyright 2006 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+window.translations=["Blog","Produse","Deocamdată nu există articole. Începeți apăsând pe ”Articol nou”.","Nu există produse în magazin. Începeți apăsând pe ”Adăugați produse”.","Nu există articole de blog. Începeți adăugând o pagină ”Blog”.","Această categorie este goală. Adăugați produse sau alegeți o altă categorie.","Galerie de imagini","Nu există imagini în galerie","Gratis","Mini","Standard","Profi","Limited","Business"];
